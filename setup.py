6ccea87b00c1ffb865be34b5168c76a0534cd7e2
akash-bluebinaries <akash.dash@bluebinaries.com>
2024-07-02 02:21:26 +0530

    setup

--- setup.py
+++ setup.py
@@ -19,10 +19,12 @@ def get_requirements(file_path:str)->List[str]:
 
 
 setup(
-    name = 'mlproject',
+
+name = 'mlproject',
     version = '0.0.1',
     author = 'akash',
     author_email='akash.dash@bluebinaries.com',
     packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
+
 )
